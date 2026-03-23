1056c113c526d68d6a2f13f0c28bf3a2e0fde950
Sasha DiVall <sashadivall@gmail.com>
2026-03-23 12:11:06 -0400

    preprocessor done

--- src/preprocessors.py
+++ src/preprocessors.py
@@ -1,4 +1,10 @@
 import nltk
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+nltk.download('punkt', quiet=True)
+nltk.download('punkt_tab', quiet=True)
+nltk.download('stopwords', quiet=True)
 
 
 class GenericPreprocessor(object):
@@ -32,14 +38,16 @@ class BOWPreprocessor(GenericPreprocessor):
 
 
 class TFIDFPreprocessor(GenericPreprocessor):
-    def __init__(self, data):
+    def __init__(self, data: list[str]):
         GenericPreprocessor.__init__(self)
         self.data = data
+        self.vectorizer = TfidfVectorizer(stop_words='english')
+
 
     def process_data(self):
         # implement TFIDF preprocessor
         # return processed data
-        pass 
+        return self.vectorizer.fit_transform(self.data)
 
 class NGramPreprocessor(GenericPreprocessor):
     def __init__(self, data, n):
@@ -51,3 +59,21 @@ class NGramPreprocessor(GenericPreprocessor):
         # implement n-gram preprocessor
         # return processed data
         pass 
+
+
+def main():
+    # sanity check for each preprocessor
+    true_df = pd.read_csv("../data/True.csv")
+    lines = list(true_df['title'])
+
+    preprocessor = TFIDFPreprocessor(lines)
+    print(len(preprocessor.data))
+
+    # process data
+    processed_data = preprocessor.process_data()
+    print(processed_data.shape)
+    print(processed_data)
+
+
+if __name__ == "__main__":
+    main()
