a6ad7757d022636ddb03ddc6a13018a66989ab8d
Sasha DiVall <sashadivall@gmail.com>
2026-03-23 11:54:37 -0400

    models skeleton code

---
+++ src/models.py
@@ -0,0 +1,68 @@
+from sklearn.linear_model import LogisticRegression
+import torch
+
+
+class GenericClfModel(object):
+
+    def train(self):
+        raise Exception("implement me in child classes")
+    
+    def predict(self):
+        raise Exception("implement me in child classes")
+    
+
+
+class NaiveBayesClf(GenericClfModel):
+
+    def __init__(self):
+        GenericClfModel.__init__()
+
+    def train(self):
+        # training loop for naive bayes clf 
+        pass
+
+    def predict(self):
+        # predictions for naive bayes
+        pass 
+
+
+class LogRegClf(GenericClfModel):
+
+    def __init__(self):
+        GenericClfModel.__init__()
+        # example...
+        self.model = LogisticRegression()
+
+    def train(self):
+        # training for log reg 
+        pass
+
+    def predict(self):
+        # prediction for log reg
+        pass 
+
+
+class NeuralNetworkClf(GenericClfModel):
+    def __init__(self):
+        GenericClfModel.__init__()
+
+    def train(self):
+        # training for NN
+        pass
+
+    def predict(self):
+        # prediction for NN
+        pass
+
+
+class MLPClf(GenericClfModel):
+    def __init__(self):
+        GenericClfModel.__init__()
+
+    def train(self):
+        # training for MLP
+        pass 
+
+    def predict(self):
+        # prediction for MLP
+        pass
