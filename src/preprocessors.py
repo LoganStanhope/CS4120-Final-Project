39df1442fc216fb17c429e56f56c5e40d29d84cf
LoganStanhope <lastanhope@outlook.com>
2026-03-23 13:12:22 -0400

    BOW impl

--- src/preprocessors.py
+++ src/preprocessors.py
@@ -1,5 +1,6 @@
 import nltk
-
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
 
 class GenericPreprocessor(object):
   
@@ -21,14 +22,36 @@ class SimplePreprocessor(GenericPreprocessor):
 
 
 class BOWPreprocessor(GenericPreprocessor):
-    def __init__(self, data):
+    """BoW preprocessor using sklearn's CountVectorizer."""
+    def __init__(self, data, max_features=20_000, min_df=3, max_df=0.95, binary=False):
         GenericPreprocessor.__init__(self)
         self.data = data
+        self.vectorizer = CountVectorizer(
+            max_features=max_features,
+            min_df=min_df,
+            max_df=max_df,
+            binary=binary,
+            strip_accents="unicode",
+            lowercase=True,
+            token_pattern=r"(?u)\b[a-zA-Z]{2,}\b",
+        )
 
+    """
+    Fit the CountVectorizer to the data and transform it into a BoW representation.
+        Returns:
+            A numpy array of shape (n_samples, vocab_size) containing the BoW representation of the input data.
+    """
     def process_data(self):
-        # implement BOW preprocessor
-        # return processed data
-        pass 
+        return self.vectorizer.fit_transform(self.data).toarray().astype(np.float32)
+
+    def transform(self, texts):
+        """Transform unseen texts using the fitted vocabulary."""
+        return self.vectorizer.transform(texts).toarray().astype(np.float32)
+
+    """Get the size of the vocabulary learned by the CountVectorizer."""
+    @property
+    def vocab_size(self):
+        return len(self.vectorizer.vocabulary_)
 
 
 class TFIDFPreprocessor(GenericPreprocessor):
